b00b850d93ce498132b4937349cac4ec9cfad703
raisulhamim <raisulhamim.dev@gmail.com>
2025-11-27 22:31:44 +0600

    counter make by using react

---
+++ counter-jsx/src/App.jsx
@@ -0,0 +1,35 @@
+import React, { useState } from 'react';
+
+const App = () => {
+  const [count, setCount] = useState(0);
+
+  return (
+    <div className="container">
+
+      <h1 className="title">Counter {count}</h1>
+
+      <div className="btn-group">
+       
+        <button
+          disabled={count >= 10 ? true : false}
+          onClick={() => setCount(count + 1)}
+          className="btn increment"
+        >
+          Increment
+        </button>
+
+        
+        <button
+          disabled={count <= 0 ? true : false}
+          onClick={() => setCount(count - 1)}
+          className="btn decrement"
+        >
+          Decrement
+        </button>
+      </div>
+
+    </div>
+  );
+};
+
+export default App;
